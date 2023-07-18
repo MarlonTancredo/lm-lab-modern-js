c403f4478d6843258b5806f0860905e3f128e370
unknown <marlon_tancredo@hotmail.com>
2023-07-18 10:25:24 +0100

    refactor: refactoring imperative_vs_declarative

--- modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
+++ modern_javascript_exercises/imperative_vs_declarative/imperative_vs_declarative.js
@@ -2,12 +2,11 @@
 
 export function longPasswords(passwords) {
   let longPasswords = [];
-
-  for (let i = 0; i < passwords.length; i++) {
-    const password = passwords[i];
-    if (password.length >= 9) {
-      longPasswords.push(password);
+  passwords.forEach((element) => {
+    if (element.length > 9) {
+      longPasswords.push(element);
     }
-  }
+  });
+
   return longPasswords;
 }
