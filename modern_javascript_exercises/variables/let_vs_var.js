30d3507d32fe0596531515181e4c71f8f84ad278
unknown <marlon_tancredo@hotmail.com>
2023-07-18 10:02:12 +0100

    refactor: refactoring let_vs_var.js

--- modern_javascript_exercises/variables/let_vs_var.js
+++ modern_javascript_exercises/variables/let_vs_var.js
@@ -2,18 +2,34 @@
 
 // Problems with var
 
-function count() {
-  for (var i = 0; i < 5; i++) {
-    console.log(i + " inside the forLoop");
-  }
-  console.log(i + " outside the forLoop");
-}
+// function count() {
+//   for (var i = 0; i < 5; i++) {
+//     console.log(i + " inside the forLoop");
+//   }
+//   console.log(i + " outside the forLoop");
+// }
 
 // count(); // <---- uncomment me!
 
 // Another problem with var is that we can overwrite it, like this:
 
-var greeting = "Hello 😘";
-var greeting = "Not today, pal! 😤";
+// var greeting = "Hello 😘";
+// var greeting = "Not today, pal! 😤";
 
-// console.log(greeting) // <----- uncomment me!
+// console.log(greeting); // <----- uncomment me!
+
+function count() {
+  for (let i = 0; i < 5; i++) {
+    console.log(i + " inside the forLoop");
+  }
+  console.log(i + " outside the forLoop");
+}
+
+count();
+
+// Another problem with var is that we can overwrite it, like this:
+
+let greeting = "Hello 😘";
+let greeting = "Not today, pal! 😤";
+
+console.log(greeting);
